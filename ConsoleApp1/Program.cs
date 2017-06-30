423e949cf6f00ceee903efea01ed6e7370256fce
daffron <daffron92@gmail.com>
2017-07-01 02:25:06 +1200

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 7;
+            Console.WriteLine(x);
+            String input = Console.ReadLine();
+            Console.WriteLine(input);
+            Console.ReadLine();
+        }
+    }
+}
